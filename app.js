fdc0726ad56d329918548c43408d7664a8d94cd8
Wasim Zaman <wasimxaman13@gmail.com>
2024-11-15 23:47:37 +0500

    Refactor app.js to include CORS middleware with specific origins

--- app.js
+++ app.js
@@ -12,13 +12,28 @@ import response from "./utils/response.js";
 
 dotenv.config();
 
+const whitelist = [process.env.FRONTEND_URL || "http://localhost:3000"];
+
+const corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: true,
+  maxAge: 86400, // 24 hours
+};
+
 const app = express();
 const PORT = process.env.PORT || 3000;
-process.env.LOCALHOST || "http://localhost:3000";
 
 app.use(express.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-app.use(cors({ origin: ["http://localhost:3000/", "http://localhost:5173/"] }));
+app.use(cors(corsOptions));
 // If you want to change the default uploads directory, you can do so here
 app.use("/uploads", express.static(path.join(path.resolve(), "uploads")));
 
