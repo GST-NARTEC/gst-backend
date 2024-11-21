b274703bebd88e3dfbcc1665c251ca84e7e5f7ca
Eissanoor <eissaanoor@gmail.com>
2024-11-21 21:11:46 +0500

    updated

--- controllers/frontendController.js
+++ controllers/frontendController.js
@@ -328,3 +328,115 @@ export const mega_menu_categories_frontSide = async (req, res, next) => {
     next(error);
   }
 };
+const footer_menus = Joi.object({
+    parent_id: Joi.string().max(255).required(),
+    category_name_en: Joi.string().max(255).required(),
+    category_name_ar: Joi.string().max(255).required(),
+    url: Joi.string().max(255).required(),
+    status: Joi.number().required(),
+  });
+  export const getAllfooter_menus = async (req, res, next) => {
+    try {
+      const AllUNSPSC = await prisma.footer_menus.findMany({
+        orderBy: {
+          updated_at: "desc", // Order by updated_at in descending order
+        },
+      });
+  
+      res.json(AllUNSPSC);
+    } catch (error) {
+      next(error);
+    }
+  };
+  export const creatfooter_menus = async (req, res, next) => {
+    try {
+      const { error, value } = footer_menus.validate(req.body);
+      if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+      }
+      const unit = await prisma.footer_menus.create({
+        data: value,
+      });
+      res.status(201).json(unit);
+    } catch (error) {
+      next(error);
+    }
+  };
+  export const getfooter_menusById = async (req, res, next) => {
+    try {
+      // const { id } = req.params;
+      // use JOi to validate the id
+      const schema = Joi.object({
+        id: Joi.string().required(),
+      });
+      const { error } = schema.validate(req.params);
+      if (error) {
+        return next(createError(400, error.details[0].message));
+      }
+  
+      const { id } = req.params;
+  
+      const cr = await prisma.footer_menus.findUnique({
+        where: { id: id },
+      });
+      if (!cr) {
+        return next(createError(404, "footer menus not found"));
+      }
+      return res.json(cr);
+    } catch (error) {
+      next(error);
+    }
+  };
+  export const updatefooter_menus = async (req, res, next) => {
+    try {
+      const schema = Joi.object({
+        id: Joi.string().required(),
+      });
+      const { error: idError } = schema.validate(req.params);
+      if (idError) {
+        return next(createError(400, idError.details[0].message));
+      }
+  
+      const { id } = req.params;
+  
+      const { error } = footer_menus.validate(req.body);
+      if (error) {
+        return res.status(400).json({ error: error.details[0].message });
+      }
+  
+      const { parent_id, category_name_en, category_name_ar, url, status } =
+        req.body;
+      const updatedUNSPSC = await prisma.footer_menus.update({
+        where: { id: id },
+        data: {
+          parent_id,
+          category_name_en,
+          category_name_ar,
+          url,
+          status,
+        },
+      });
+  
+      res.json(updatedUNSPSC);
+    } catch (error) {
+      next(error);
+    }
+  };
+  export const deletefooter_menus = async (req, res, next) => {
+    try {
+      const schema = Joi.object({
+        id: Joi.string().required(),
+      });
+      const { error } = schema.validate(req.params);
+      if (error) {
+        return next(createError(400, error.details[0].message));
+      }
+      const { id } = req.params;
+      await prisma.footer_menus.delete({
+        where: { id: id },
+      });
+      return res.json({ message: "footer menus deleted successfully" });
+    } catch (error) {
+      next(error);
+    }
+  };
--- routes/frontendRoutes.js
+++ routes/frontendRoutes.js
@@ -1,5 +1,20 @@
 import express from "express";
-import {getAllmega_menu,createmega_menus,getmega_menusById,updatemega_menus,deletemega_menus} from "../controllers/frontendController.js";
+import {getAllmega_menu,
+    createmega_menus,
+    getmega_menusById,
+    updatemega_menus,
+    deletemega_menus,
+    getAllmega_menu_categories,
+    creatmega_menu_categories,
+    getmega_menu_categoriesById,
+    updatemega_menu_categories,
+    deletemega_menu_categories,
+    getAllfooter_menus,
+    creatfooter_menus,
+    getfooter_menusById,
+    updatefooter_menus,
+    deletefooter_menus,
+    mega_menu_categories_frontSide} from "../controllers/frontendController.js";
 import upload from "../utils/multerConfig.js";
 
 const router = express.Router();
@@ -9,5 +24,19 @@ router.get("/getAllmega_menu", getAllmega_menu);
 router.get("/getmega_menusById/:id", getmega_menusById);
 router.put("/updatemega_menus/:id", updatemega_menus);
 router.delete("/deletemega_menus/:id", deletemega_menus);
+// Routes for mega_menu_categories
+router.get("/mega_menu_categories_frontSide", mega_menu_categories_frontSide);
+router.get("/getAllmega_menu_categories", getAllmega_menu_categories);
+router.post("/creatmega_menu_categories",upload.single("image"), creatmega_menu_categories);
+router.get("/getmega_menu_categoriesById/:id", getmega_menu_categoriesById);
+router.put("/updatemega_menu_categories/:id",upload.single("image"),updatemega_menu_categories);
+router.delete("/deletemega_menu_categories/:id",deletemega_menu_categories);
+
+// Routes for footer_menus
+router.get("/getAllfooter_menus", getAllfooter_menus);
+router.post("/creatfooter_menus", creatfooter_menus);
+router.get("/getfooter_menusById/:id", getfooter_menusById);
+router.put("/updatefooter_menus/:id",updatefooter_menus);
+router.delete("/deletefooter_menus/:id", deletefooter_menus);
 
 export default router;
