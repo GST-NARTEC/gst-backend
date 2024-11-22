ac8365c2b3ed52ce8ad10274ecfb8196aeb2d22c
Eissanoor <eissaanoor@gmail.com>
2024-11-22 11:34:31 +0500

    updated

--- utils/multerConfig.js
+++ utils/multerConfig.js
@@ -1,36 +1,36 @@
-// import fs from "fs";
-// import multer from "multer";
+import fs from "fs";
+import multer from "multer";
 
-// const storage = multer.diskStorage({
-//   destination: function (req, file, cb) {
-//     let uploadPath = "uploads/";
+const storage = multer.diskStorage({
+  destination: function (req, file, cb) {
+    let uploadPath = "uploads/";
 
-//     if (file.fieldname === "image") {
-//       uploadPath += "images/";
-//     } else {
-//       uploadPath += "misc/";
-//     }
+    if (file.fieldname === "image") {
+      uploadPath += "images/";
+    } else {
+      uploadPath += "misc/";
+    }
 
-//     fs.mkdirSync(uploadPath, { recursive: true });
-//     cb(null, uploadPath);
-//   },
-//   filename: function (req, file, cb) {
-//     cb(null, Date.now() + "-" + file.originalname);
-//   },
-// });
+    fs.mkdirSync(uploadPath, { recursive: true });
+    cb(null, uploadPath);
+  },
+  filename: function (req, file, cb) {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
+});
 
-// const fileFilter = (req, file, cb) => {
-//   if (file.mimetype.startsWith("image")) {
-//     cb(null, true);
-//   } else {
-//     cb(new Error("Not an image! Please upload only images."), false);
-//   }
-// };
+const fileFilter = (req, file, cb) => {
+  if (file.mimetype.startsWith("image")) {
+    cb(null, true);
+  } else {
+    cb(new Error("Not an image! Please upload only images."), false);
+  }
+};
 
-// const upload = multer({
-//   storage: storage,
-//   fileFilter: fileFilter,
-//   limits: { fileSize: 1024 * 1024 * 10 }, // 5MB file size limit
-// });
+const upload = multer({
+  storage: storage,
+  fileFilter: fileFilter,
+  limits: { fileSize: 1024 * 1024 * 10 }, // 5MB file size limit
+});
 
-// export default upload;
+export default upload;
