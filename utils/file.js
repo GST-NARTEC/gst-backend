fada01203a599bddc374385eda0d00f6cbd37af7
Wasim Zaman <wasimxaman13@gmail.com>
2024-11-23 14:36:12 +0500

    Refactor file.js to handle file deletion and throw an error if deletion fails

--- utils/file.js
+++ utils/file.js
@@ -3,6 +3,7 @@ import path, { dirname } from "path";
 import { fileURLToPath } from "url";
 
 import config from "../config/config.js";
+import MyError from "./error.js";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
@@ -15,13 +16,14 @@ export const addDomain = (imagePath) => {
 export const deleteFile = (imageUrl) => {
   if (!imageUrl) return Promise.resolve();
 
-  const imagePath = imageUrl.replace(config.DOMAIN, "").replace(/^\//, "");
+  const imagePath = imageUrl.replace(config.DOMAIN, "");
   const fullPath = path.join(__dirname, "..", imagePath);
 
   return new Promise((resolve) => {
     fs.unlink(fullPath, (err) => {
       if (err) {
         console.error("Failed to delete image:", err);
+        throw MyError("Failed to delete file");
       }
       resolve();
     });
