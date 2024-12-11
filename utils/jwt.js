131740380f3581a6e438940801235774a78a3ed2
Wasim Zaman <wasimxaman13@gmail.com>
2024-12-11 20:16:16 +0500

    updated token management

--- utils/jwt.js
+++ utils/jwt.js
@@ -23,6 +23,26 @@ class JWT {
   static verifyToken(token) {
     return jwt.verify(token, process.env.JWT_SECRET);
   }
+
+  static generateAccessToken(payload) {
+    return jwt.sign(payload, config.JWT_ACCESS_SECRET, {
+      expiresIn: config.JWT_ACCESS_EXPIRY,
+    });
+  }
+
+  static generateRefreshToken(payload) {
+    return jwt.sign(payload, config.JWT_REFRESH_SECRET, {
+      expiresIn: config.JWT_REFRESH_EXPIRY,
+    });
+  }
+
+  static verifyAccessToken(token) {
+    return jwt.verify(token, config.JWT_ACCESS_SECRET);
+  }
+
+  static verifyRefreshToken(token) {
+    return jwt.verify(token, config.JWT_REFRESH_SECRET);
+  }
 }
 
 export default JWT;
