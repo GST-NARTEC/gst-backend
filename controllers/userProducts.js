0aeab0bc56afbc01276c60960b9c01c6de30f7bf
Wasim Zaman <wasimxaman13@gmail.com>
2024-12-20 18:19:15 +0500

    changed from queue to normal

--- controllers/userProducts.js
+++ controllers/userProducts.js
@@ -1,4 +1,3 @@
-import { userProductQueue } from "../config/queue.js";
 import { userProductSchema } from "../schemas/userProductSchema.js";
 import MyError from "../utils/error.js";
 import { addDomain, deleteFile } from "../utils/file.js";
@@ -49,95 +48,89 @@ class UserProductsController {
 
       let randomGtin, product;
 
-      // add job to the queue
-      await userProductQueue.add("user-product", {
-        orders,
-        productData,
-      });
+      // now check each order one by one and check for available gtins in each order, if any order has available gtins, then pick one at random and create the product
+      for (const order of orders) {
+        console.log("order id", order.id);
 
-      //   // now check each order one by one and check for available gtins in each order, if any order has available gtins, then pick one at random and create the product
-      //   for (const order of orders) {
-      //     console.log("order id", order.id);
+        // find available gtin in the order
+        const availableGtins = order.assignedGtins.filter(
+          (gtin) => gtin.gtin?.status === "Sold"
+        );
 
-      //     // find available gtin in the order
-      //     const availableGtins = order.assignedGtins.filter(
-      //       (gtin) => gtin.gtin?.status === "Sold"
-      //     );
+        if (availableGtins.length < 1) {
+          console.log("No gtin is free in a specific order" + order.id);
+          // change isSec to false in case if no gtin is free in a specific order
+          await prisma.order.update({
+            where: {
+              id: order.id,
+            },
+            data: {
+              isSec: false,
+            },
+          });
+          continue;
+        }
 
-      //     if (availableGtins.length < 1) {
-      //       console.log("No gtin is free in a specific order" + order.id);
-      //       // change isSec to false in case if no gtin is free in a specific order
-      //       await prisma.order.update({
-      //         where: {
-      //           id: order.id,
-      //         },
-      //         data: {
-      //           isSec: false,
-      //         },
-      //       });
-      //       continue;
-      //     }
+        // check if it is last gtin of the order
+        if (availableGtins.length === 1) {
+          console.log("Last gtin of the order" + order.id);
+          // update the isSec to false for this specific order
+          await prisma.order.update({
+            where: {
+              id: order.id,
+            },
+            data: {
+              isSec: false,
+            },
+          });
+          continue;
+        }
 
-      //     // check if it is last gtin of the order
-      //     if (availableGtins.length === 1) {
-      //       console.log("Last gtin of the order" + order.id);
-      //       // update the isSec to false for this specific order
-      //       await prisma.order.update({
-      //         where: {
-      //           id: order.id,
-      //         },
-      //         data: {
-      //           isSec: false,
-      //         },
-      //       });
-      //       continue;
-      //     }
+        if (availableGtins.length > 0) {
+          console.log("Available gtin in a specific order" + order.id);
+          // pick a random gtin from available gtins
+          randomGtin =
+            availableGtins[Math.floor(Math.random() * availableGtins.length)];
 
-      //     if (availableGtins.length > 0) {
-      //       console.log("Available gtin in a specific order" + order.id);
-      //       // pick a random gtin from available gtins
-      //       randomGtin =
-      //         availableGtins[Math.floor(Math.random() * availableGtins.length)];
+          // Handle image uploads
+          const imageUrls = [];
+          if (req.files?.images) {
+            for (const file of req.files.images) {
+              const imagePath = addDomain(file.path);
+              imageUrls.push(imagePath);
+              imagePaths.push(imagePath);
+            }
+          }
 
-      //       // Handle image uploads
-      //       const imageUrls = [];
-      //       if (req.files?.images) {
-      //         for (const file of req.files.images) {
-      //           const imagePath = addDomain(file.path);
-      //           imageUrls.push(imagePath);
-      //           imagePaths.push(imagePath);
-      //         }
-      //       }
+          // Create product
+          product = await prisma.userProduct.create({
+            data: {
+              ...productData,
+              gtin: randomGtin.gtin.gtin,
+              userId: req.user.id,
+              images: {
+                create: imageUrls.map((url) => ({ url })),
+              },
+            },
+            include: {
+              images: true,
+            },
+          });
 
-      //       // Create product
-      //       product = await prisma.userProduct.create({
-      //         data: {
-      //           ...productData,
-      //           gtin: randomGtin.gtin.gtin,
-      //           userId: req.user.id,
-      //           images: {
-      //             create: imageUrls.map((url) => ({ url })),
-      //           },
-      //         },
-      //         include: {
-      //           images: true,
-      //         },
-      //       });
+          console.log("Product created" + product.id);
 
-      //       console.log("Product created" + product.id);
+          // Update GTIN status to "Used"
+          await prisma.gTIN.update({
+            where: { gtin: randomGtin.gtin.gtin, status: "Sold" },
+            data: {
+              status: "Used",
+            },
+          });
 
-      //       // Update GTIN status to "Used"
-      //       await prisma.gTIN.update({
-      //         where: { gtin: randomGtin.gtin.gtin, status: "Sold" },
-      //         data: {
-      //           status: "Used",
-      //         },
-      //       });
-
-      //       console.log("GTIN status updated to Used" + randomGtin.gtin.gtin);
-      //       break;
-      //     }
-      //   }
+          console.log("GTIN status updated to Used" + randomGtin.gtin.gtin);
+          break;
+        }
+      }
 
       //   // Handle image uploads
       //   const imageUrls = [];
