b5fc6f1a69c31fb412f11bebe7b52c577a60b5f6
Wasim Zaman <wasimxaman13@gmail.com>
2024-12-24 10:31:51 +0500

    deleted some files and updated sentry config

--- instrument.js
+++ instrument.js
@@ -2,7 +2,7 @@ import * as Sentry from "@sentry/node";
 import { nodeProfilingIntegration } from "@sentry/profiling-node";
 
 Sentry.init({
-  dsn: "https://d386905c0c46b64814dba80f16b49e51@o4508110398095360.ingest.us.sentry.io/4508505052282880",
+  dsn: process.env.SENTRY_DSN,
   integrations: [nodeProfilingIntegration()],
   tracesSampleRate: 1.0,
 });
