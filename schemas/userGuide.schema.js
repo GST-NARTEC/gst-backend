af6e4f83231b3297fc3a62cb51467221258bd85b
Wasim Zaman <wasimxaman13@gmail.com>
2024-12-31 12:28:28 +0500

    Refactor userGuide.schema.js to make description fields optional

--- schemas/userGuide.schema.js
+++ schemas/userGuide.schema.js
@@ -3,7 +3,7 @@ import Joi from "joi";
 export const userGuideSchema = Joi.object({
   titleEn: Joi.string().required(),
   titleAr: Joi.string().required(),
-  descriptionEn: Joi.string().required(),
-  descriptionAr: Joi.string().required(),
+  descriptionEn: Joi.string().optional(),
+  descriptionAr: Joi.string().optional(),
   type: Joi.string().required().valid("pdf", "video"),
 }).unknown(true);
