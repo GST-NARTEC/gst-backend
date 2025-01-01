628b9ab9364495d96c635c6ff56304ba5febc3b1
Wasim Zaman <wasimxaman13@gmail.com>
2025-01-01 12:40:19 +0500

    user deletion worker updated #1

--- workers/userWorker.js
+++ workers/userWorker.js
@@ -28,10 +28,13 @@ const processUserDeletion = async (job) => {
               addonItems: true,
             },
           },
-          assignedGtins: true,
+          assignedGtins: {
+            include: {
+              gtin: true,
+            },
+          },
         },
       },
-      invoices: true,
       products: {
         include: {
           images: true,
@@ -66,17 +69,24 @@ const processUserDeletion = async (job) => {
 
     // Process orders
     for (const order of user.orders) {
-      // release GTINs
-      if (order.assignedGtins && order.assignedGtins?.length > 0) {
-        for (const gtin of order.assignedGtins) {
-          await prisma.gTIN?.update({
-            where: { gtin: gtin.gtin },
-            data: { status: "Available" },
-          });
+      // Release GTINs
+      if (order.assignedGtins?.length > 0) {
+        for (const assignedGtin of order.assignedGtins) {
+          if (assignedGtin.gtin) {
+            await prisma.gTIN.update({
+              where: {
+                id: assignedGtin.gtin.id,
+                gtin: assignedGtin.gtin.gtin,
+              },
+              data: {
+                status: "Available",
+              },
+            });
+          }
         }
       }
 
-      // Delete GTINs first
+      // Delete GTINs assignments
       await prisma.assignedGtin.deleteMany({
         where: { orderId: order.id },
       });
@@ -95,14 +105,9 @@ const processUserDeletion = async (job) => {
       // Delete files and invoice
       if (order.invoice?.pdf) {
         await deleteFile(order.invoice.pdf);
-        // delete invoice, but lets check if it exists first
-        const invoice = await prisma.invoice.findUnique({
+        await prisma.invoice.delete({
           where: { id: order.invoice.id },
         });
-        if (invoice) {
-          await prisma.invoice.delete({ where: { id: order.invoice.id } });
-        }
-        // await prisma.invoice.delete({ where: { id: order.invoice.id } });
       }
 
       if (order.receipt) await deleteFile(order.receipt);
@@ -113,7 +118,7 @@ const processUserDeletion = async (job) => {
     }
 
     // Process user products
-    if (user.products && user.products?.length > 0) {
+    if (user.products?.length > 0) {
       for (const product of user.products) {
         // Delete product images from storage
         for (const image of product.images) {
@@ -121,16 +126,17 @@ const processUserDeletion = async (job) => {
         }
 
         // Release GTIN if exists
-        if (product?.gtin) {
-          console.log("Releasing GTIN", product.gtin);
-          // Add a check if GTIN exists in the database
-          const existingGtin = await prisma.gTIN?.findFirst({
+        if (product.gtin) {
+          const existingGtin = await prisma.gTIN.findUnique({
             where: { gtin: product.gtin },
           });
 
           if (existingGtin) {
-            await prisma.gTIN?.update({
-              where: { gtin: product.gtin },
+            await prisma.gTIN.update({
+              where: {
+                id: existingGtin.id,
+                gtin: existingGtin.gtin,
+              },
               data: {
                 status: "Available",
               },
@@ -159,24 +165,24 @@ const processUserDeletion = async (job) => {
       await prisma.cart.delete({ where: { id: user.cart.id } });
     }
 
-    // delete user docs
-    if (user.docs && user.docs?.length > 0) {
+    // Delete user docs
+    if (user.docs?.length > 0) {
       for (const doc of user.docs) {
         await deleteFile(doc.doc);
         await prisma.userDoc.delete({ where: { id: doc.id } });
       }
     }
 
-    // delete user brands
-    if (user.brands && user.brands?.length > 0) {
+    // Delete user brands
+    if (user.brands?.length > 0) {
       for (const brand of user.brands) {
         await deleteFile(brand.document);
         await prisma.brand.delete({ where: { id: brand.id } });
       }
     }
 
-    // delete user help tickets
-    if (user.helpTickets && user.helpTickets?.length > 0) {
+    // Delete user help tickets
+    if (user.helpTickets?.length > 0) {
       for (const ticket of user.helpTickets) {
         if (ticket.doc) await deleteFile(ticket.doc);
         await prisma.helpTicket.delete({ where: { id: ticket.id } });
