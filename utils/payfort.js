c061015c390197c57948897e88f4a0e0e5a6f583
Wasim Zaman <wasimxaman13@gmail.com>
2025-01-08 18:43:50 +0500

    Refactor return URL in PayfortService class

--- utils/payfort.js
+++ utils/payfort.js
@@ -75,7 +75,7 @@ class PayfortService {
       currency: orderData.currency || "SAR",
       language: this.config.language,
       customer_email: orderData.email,
-      return_url: `${process.env.FRONTEND_URL}/success`,
+      return_url: `https://buybarcodeupc.com/success`,
     };
 
     // Generate signature
