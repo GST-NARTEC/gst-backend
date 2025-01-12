4c7b262a3c2bbbfd98ea3932cf6a7bd02dae2ab8
Wasim Zaman <wasimxaman13@gmail.com>
2025-01-12 19:12:51 +0500

    Refactor redirect logic in payment.js and fix success payment route

--- routes/payment.js
+++ routes/payment.js
@@ -3,10 +3,10 @@ import PaymentController from "../controllers/payment.js";
 
 const router = express.Router();
 
-router.post("/initialize", PaymentController.initPayment);
 router.all("/success", (req, res, next) => {
   console.log("Hit: /payment/success");
   return res.redirect(`https://buybarcodeupc.com/payment/success?status=error`);
 });
+router.post("/initialize", PaymentController.initPayment);
 
 export default router;
