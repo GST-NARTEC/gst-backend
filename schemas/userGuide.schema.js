bbe1d6cb0a10a5cd2a682df220142e452c06b2a2
Wasim Zaman <wasimxaman13@gmail.com>
2025-07-31 14:00:31 +0500

    fix: Make title fields optional in user guide schema

--- schemas/userGuide.schema.js
+++ schemas/userGuide.schema.js
@@ -1,8 +1,8 @@
 import Joi from "joi";
 
 export const userGuideSchema = Joi.object({
-  titleEn: Joi.string().required(),
-  titleAr: Joi.string().required(),
+  titleEn: Joi.string(),
+  titleAr: Joi.string(),
   descriptionEn: Joi.string().optional(),
   descriptionAr: Joi.string().optional(),
   type: Joi.string().required().valid("pdf", "video"),
