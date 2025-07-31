bdfd9e0d610451af7af2960c2b97ddb5592ab64c
Wasim Zaman <wasimxaman13@gmail.com>
2025-07-31 14:06:12 +0500

    fix: Require title fields in user guide schema

--- schemas/userGuide.schema.js
+++ schemas/userGuide.schema.js
@@ -1,8 +1,8 @@
 import Joi from "joi";
 
 export const userGuideSchema = Joi.object({
-  titleEn: Joi.string(),
-  titleAr: Joi.string(),
+  titleEn: Joi.string().required(),
+  titleAr: Joi.string().required(),
   descriptionEn: Joi.string().optional(),
   descriptionAr: Joi.string().optional(),
   type: Joi.string().required().valid("pdf", "video"),
